5c14c90172002fe0e50b2105f3ab50ef87d5aec8
Chris <chris.woodrow@serli.com>
2014-12-03 09:41:37 +0100

    correction erreur de compile

--- src/main/java/com/formation/controller/ApplicationController.java
+++ src/main/java/com/formation/controller/ApplicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Chris on 23/10/14.
  */
 @Controller
-public class ApplicationController {frgreveds
+public class ApplicationController {
     @RequestMapping("/")
     public String getHome(){
         return "redirect:"+ UserController.USERS_LIST;
